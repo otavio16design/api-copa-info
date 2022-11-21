1fe9a280fdb38a2699e944ea2a515bfbe7e69911
unknown <om16design@gmail.com>
2022-11-21 08:22:57 -0300

    teste

--- jogos/php-rest-curl.php
+++ jogos/php-rest-curl.php
@@ -5,20 +5,21 @@
  * https://github.com/jmoraleda/php-rest-curl
  * (c) 2014 Jordi Moraleda
  */
+class RestCurl
+{
+  public static function exec($method, $url, $obj = array())
+  {
 
-Class RestCurl {
-  public static function exec($method, $url, $obj = array()) {
-     
     $curl = curl_init();
-     
-    switch($method) {
+
+    switch ($method) {
       case 'GET':
-        if(strrpos($url, "?") === FALSE) {
+        if (strrpos($url, "?") === FALSE) {
           $url .= '?' . http_build_query($obj);
         }
         break;
 
-      case 'POST': 
+      case 'POST':
         curl_setopt($curl, CURLOPT_POST, TRUE);
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($obj));
         break;
@@ -32,41 +33,45 @@ Class RestCurl {
 
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-        'Accept: application/json',
-        'Content-Type: application/json',
-        "Accept: */*",
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzc1OTUyMWZkOWFhYzIyNjcyYTFkYTkiLCJpYXQiOjE2Njg5NTE0ODcsImV4cCI6MTY2OTAzNzg4N30.HW5QpNwKCScpi5dMegvO_1JKVzWe6NPVmH37uEGuQZ8",
-        "User-Agent: Thunder Client (https://www.thunderclient.com)",
-    )); 
+      'Accept: application/json',
+      'Content-Type: application/json',
+      "Accept: */*",
+      "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mzc1OTUyMWZkOWFhYzIyNjcyYTFkYTkiLCJpYXQiOjE2NjkwMjk3NTAsImV4cCI6MTY2OTExNjE1MH0.b0BpF_M3Fo_FtBED5lq2dB0MjWu80Yhl-hCIo0gyTYs",
+      "User-Agent: Thunder Client (https://www.thunderclient.com)",
+    ));
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HEADER, TRUE);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, TRUE);
-    
+
     // Exec
     $response = curl_exec($curl);
     $info = curl_getinfo($curl);
     curl_close($curl);
-    
+
     // Data
     $header = trim(substr($response, 0, $info['header_size']));
     $body = substr($response, $info['header_size']);
-     
+
     return array('status' => $info['http_code'], 'header' => $header, 'data' => json_decode($body));
   }
 
-  public static function get($url, $obj = array()) {
-     return RestCurl::exec("GET", $url, $obj);
+  public static function get($url, $obj = array())
+  {
+    return RestCurl::exec("GET", $url, $obj);
   }
 
-  public static function post($url, $obj = array()) {
-     return RestCurl::exec("POST", $url, $obj);
+  public static function post($url, $obj = array())
+  {
+    return RestCurl::exec("POST", $url, $obj);
   }
 
-  public static function put($url, $obj = array()) {
-     return RestCurl::exec("PUT", $url, $obj);
+  public static function put($url, $obj = array())
+  {
+    return RestCurl::exec("PUT", $url, $obj);
   }
 
-  public static function delete($url, $obj = array()) {
-     return RestCurl::exec("DELETE", $url, $obj);
+  public static function delete($url, $obj = array())
+  {
+    return RestCurl::exec("DELETE", $url, $obj);
   }
 }
